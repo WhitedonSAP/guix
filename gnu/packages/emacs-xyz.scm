73bf5520596fda4138a7a815ac559728fb6bf331
WhitedonSAP <ayrtonarantes0987654321ayrt008@gmail.com>
2024-03-18 08:46:03 -0300

    update files

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -8958,7 +8958,14 @@ user.")
     (arguments
      (list
       #:tests? #t
-      #:test-command #~(list "make" "test-only")))
+      #:test-command #~(list "make" "test-only")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'compatibility-with-recent-buttercup
+            (lambda _
+              (substitute* (find-files "tests/" "\\.el$")
+                (("\\(buttercup-minor-mode\\) -\\*-")
+                 "(buttercup-minor-mode); lexical-binding: t -*-")))))))
     (native-inputs (list emacs-buttercup))
     (build-system emacs-build-system)
     (home-page "https://elpa.nongnu.org/nongnu/subed.html")
@@ -27764,8 +27771,17 @@ turn.")
       (native-inputs
        (list emacs-buttercup emacs-undercover))
       (arguments
-       `(#:tests? #t
-         #:test-command '("buttercup" "-L" "test/github-review-test.el")))
+       (list
+        #:tests? #t
+        #:test-command #~(list "buttercup" "-L" "test/github-review-test.el")
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-after 'unpack 'compatibility-with-recent-buttercup
+              (lambda _
+                (emacs-batch-edit-file "test/github-review-test.el"
+                  '(progn
+                    (insert ";;; -*-lexical-binding:t-*-")
+                    (basic-save-buffer))))))))
       (home-page "https://github.com/charignon/github-review")
       (synopsis "Review GitHub pull requests within Emacs")
       (description "This package provides commands to pull in, comment on, and
@@ -37911,44 +37927,46 @@ released, and track their progress in watching a series.")
     (license license:gpl3+)))
 
 (define-public emacs-webpaste
-  (package
-    (name "emacs-webpaste")
-    (version "3.2.2")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/etu/webpaste.el")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "07hj9nr7x6c9w2dnvc58cfbprgp9cqzdxflp5qlpglzdw0bi9s3c"))))
-    (build-system emacs-build-system)
-    (arguments
-     `(#:tests? #t
-       #:test-command '("make" "unit" "integration")
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'fix-tests
-           (lambda _
-             ;; Do not use cask to run tests.
-             (substitute* "Makefile"
-               (("\\$\\{CASK\\} exec ") ""))
-             ;; Disable tests that need network access.
-             (substitute* (list "tests/unit/test-webpaste-provider-creation.el"
-                                "tests/integration/test-webpaste-providers.el")
-               (("describe") "xdescribe")))))))
-    (native-inputs
-     (list emacs-buttercup))
-    (propagated-inputs
-     (list emacs-request))
-    (home-page "https://github.com/etu/webpaste.el")
-    (synopsis "Paste to pastebin-like services")
-    (description "This mode pastes whole buffers or parts of buffers
+  (let ((commit "8ac7b2d409f158bcaa853aa1c5763e8acf2857bb")
+        (revision "1"))
+    (package
+      (name "emacs-webpaste")
+      (version (git-version "3.2.2" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/etu/webpaste.el")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1wl2q4q7c2a1m60q4dnajsyzkc9yprkyzx4hxzmxkwry22k906x3"))))
+      (build-system emacs-build-system)
+      (arguments
+       `(#:tests? #t
+         #:test-command '("make" "unit" "integration")
+         #:phases
+         (modify-phases %standard-phases
+           (add-after 'unpack 'fix-tests
+             (lambda _
+               ;; Do not use cask to run tests.
+               (substitute* "Makefile"
+                 (("\\$\\{CASK\\} exec ") ""))
+               ;; Disable tests that need network access.
+               (substitute* (list "tests/unit/test-webpaste-provider-creation.el"
+                                  "tests/integration/test-webpaste-providers.el")
+                 (("describe") "xdescribe")))))))
+      (native-inputs
+       (list emacs-buttercup))
+      (propagated-inputs
+       (list emacs-request))
+      (home-page "https://github.com/etu/webpaste.el")
+      (synopsis "Paste to pastebin-like services")
+      (description "This mode pastes whole buffers or parts of buffers
 to pastebin-like services.  It supports more than one service and will
 failover if one service fails.  More services can easily be added over time
 and preferred services can easily be configured.")
-    (license license:gpl3+)))
+      (license license:gpl3+))))
 
 (define-public emacs-keystore-mode
   (let ((release "0.0.1")
