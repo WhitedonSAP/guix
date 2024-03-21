075eb7754c72b262e4420ce18dfb5e72f480bb3c
WhitedonSAP <ayrtonarantes0987654321ayrt008@gmail.com>
2024-03-20 23:02:57 -0300

    update files

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -5875,8 +5875,8 @@ code written in the D programming language.")
 
 (define-public emacs-extempore-mode
   ;; Use the latest commit at time of packaging.  There are no releases or tags.
-  (let ((commit "09518ae6650d7be33a4633a4c0f31b7130d04c6e")
-        (revision "1"))
+  (let ((commit "92e0fff482a0a4dc2971c39581c5ea9e84ae5e1c")
+        (revision "2"))
     (package
       (name "emacs-extempore-mode")
       (version (git-version "0.0.0" revision commit))
@@ -5888,7 +5888,7 @@ code written in the D programming language.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0hfza9lzdsz94gxhmzyp9fwviscv19rmnjgd0q613faayn11sjsp"))))
+          (base32 "1chxl2x9wjblhfknx7aa8pgqppc60917n437bxapx4hp3275x16q"))))
       (build-system emacs-build-system)
       (home-page "https://github.com/extemporelang/extempore-emacs-mode")
       (synopsis "Emacs major mode for Extempore source files")
@@ -10363,70 +10363,68 @@ in HTML mode.")
 
 (define-public emacs-slime
   ;; Update together with sbcl-slime-swank.
-  (let ((commit "735258a26bb97e85d25f39e4bef83c1f80c12f5d")
-        (revision "1"))
-    (package
-      (name "emacs-slime")
-      (version (git-version "2.28" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/slime/slime")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0prskgzfqjmn8sc7p9nklnd0n1plwcvh40slgh23km31raplmzk7"))))
-      (build-system emacs-build-system)
-      (arguments
-       (list
-        #:include #~(cons* "\\.lisp$" "\\.asd$"
-                           "contrib"
-                           "lib/hyperspec.el"
-                           %default-include)
-        #:exclude #~(list "^slime-tests.el" "^contrib/test/"
-                          "^contrib/Makefile$" "^contrib/README.md$")
-        #:phases
-        #~(modify-phases %standard-phases
-            (add-before 'install 'configure
-              (lambda* _
-                (emacs-substitute-variables "slime.el"
-                  ("inferior-lisp-program" "sbcl"))))
-            (add-before 'install 'install-doc
-              (lambda _
-                (let ((info-dir (string-append #$output "/share/info"))
-                      (doc-dir (string-append #$output "/share/doc/"
-                                              #$name "-" #$version))
-                      (doc-files '("doc/slime-refcard.pdf"
-                                   "README.md" "NEWS" "PROBLEMS"
-                                   "CONTRIBUTING.md")))
-                  (with-directory-excursion "doc"
-                    (substitute* "Makefile"
-                      (("infodir=/usr/local/info")
-                       (string-append "infodir=" info-dir)))
-                    (invoke "make" "html/index.html")
-                    (invoke "make" "slime.info")
-                    (install-file "slime.info" info-dir)
-                    (copy-recursively "html" (string-append doc-dir "/html")))
-                  (for-each (lambda (f)
-                              (install-file f doc-dir)
-                              (delete-file f))
-                            doc-files)
-                  (delete-file-recursively "doc")))))))
-      (propagated-inputs
-       (list emacs-macrostep))
-      (native-inputs
-       (list texinfo))
-      (home-page "https://github.com/slime/slime")
-      (synopsis "Superior Lisp Interaction Mode for Emacs")
-      (description
-       "SLIME extends Emacs with support for interactive programming in
+  (package
+    (name "emacs-slime")
+    (version "2.29.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/slime/slime")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1fcasqsdfwsphkfx1vd3r5mr89vgvzb9g2dbw82mc9lijg1mr1ki"))))
+    (build-system emacs-build-system)
+    (arguments
+     (list
+      #:include #~(cons* "\\.lisp$" "\\.asd$"
+                         "contrib"
+                         "lib/hyperspec.el"
+                         %default-include)
+      #:exclude #~(list "^slime-tests.el" "^contrib/test/"
+                        "^contrib/Makefile$" "^contrib/README.md$")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'install 'configure
+            (lambda* _
+              (emacs-substitute-variables "slime.el"
+                ("inferior-lisp-program" "sbcl"))))
+          (add-before 'install 'install-doc
+            (lambda _
+              (let ((info-dir (string-append #$output "/share/info"))
+                    (doc-dir (string-append #$output "/share/doc/"
+                                            #$name "-" #$version))
+                    (doc-files '("doc/slime-refcard.pdf"
+                                 "README.md" "NEWS" "PROBLEMS"
+                                 "CONTRIBUTING.md")))
+                (with-directory-excursion "doc"
+                  (substitute* "Makefile"
+                    (("infodir=/usr/local/info")
+                     (string-append "infodir=" info-dir)))
+                  (invoke "make" "html/index.html")
+                  (invoke "make" "slime.info")
+                  (install-file "slime.info" info-dir)
+                  (copy-recursively "html" (string-append doc-dir "/html")))
+                (for-each (lambda (f)
+                            (install-file f doc-dir)
+                            (delete-file f))
+                          doc-files)
+                (delete-file-recursively "doc")))))))
+    (propagated-inputs
+     (list emacs-macrostep))
+    (native-inputs
+     (list texinfo))
+    (home-page "https://github.com/slime/slime")
+    (synopsis "Superior Lisp Interaction Mode for Emacs")
+    (description
+     "SLIME extends Emacs with support for interactive programming in
 Common Lisp.  The features are centered around @command{slime-mode},
 an Emacs minor mode that complements the standard @command{lisp-mode}.
 While lisp-mode supports editing Lisp source files, @command{slime-mode}
 adds support for interacting with a running Common Lisp process
 for compilation, debugging, documentation lookup, and so on.")
-      (license (list license:gpl2+ license:public-domain)))))
+    (license (list license:gpl2+ license:public-domain))))
 
 (define-public emacs-popup
   (package
@@ -16323,7 +16321,7 @@ extensions.")
 (define-public emacs-evil-collection
   (package
     (name "emacs-evil-collection")
-    (version "0.0.9")
+    (version "0.0.10")
     (source
      (origin
        (method git-fetch)
@@ -16332,7 +16330,7 @@ extensions.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1y1ig4shqaaiiwqm5pv8hvh8ynr6irhffkgmpyzmhdaaicxnfazc"))))
+        (base32 "09hnxb8nh3g0hi93fz9f1y164gv9iyh5994wfn6fsq2v1xdz8phm"))))
     (build-system emacs-build-system)
     (arguments
      (list
